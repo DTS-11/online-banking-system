078c5590dd0181d6fcf31a9196a47425648409fb
DTS <officialdts11@gmail.com>
2023-08-30 22:50:27 +0530

    made some major changes

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import sys
+
 import mysql.connector as db
+import tabulate
 
 
 conn = db.connect(
@@ -10,7 +13,6 @@ conn = db.connect(
 cur = conn.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS BankUsers (userId INT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(25) UNIQUE, password VARCHAR(20))")
 cur.execute("CREATE TABLE IF NOT EXISTS BankAccount (accountId INT PRIMARY KEY AUTO_INCREMENT, userId INT, username VARCHAR(25), balance INT)")
-cur.execute("CREATE TABLE IF NOT EXISTS BankTransactions (transactionId INT PRIMARY KEY AUTO_INCREMENT, userId INT, withdrawed INT, deposited INT)")
 
 
 def signUp():
@@ -23,32 +25,66 @@ def signUp():
 
         if result[0] > 0: # type: ignore
             print("Username is already taken. Please enter a new unique username.")
+
         else:
             cur.execute(f"INSERT INTO BankUsers (username, password) VALUES ('{username}', '{password}')")
             print("Sign-up successful!")
-
-            cur.execute(f"INSERT INTO BankAccount (amount) VALUES (500) WHERE username = '{username}'")
-            print("As a head start, we've deposited 500 into your account!")
             conn.commit()
-            break
+
+            cur.execute(f"SELECT userId FROM BankUsers WHERE username = '{username}'")
+            user_id = cur.fetchone()[0]
+
+            cur.execute(f"INSERT INTO BankAccount (userId, username, balance) VALUES ({user_id}, '{username}', 500)")
+            conn.commit()
+
+            print("As a head start, we've deposited 500 into your account!")
+            print("Thank you for choosing Digital Bank Ltd.")
+
+        break
 
 
 def signIn():
     for i in range(3):
         username = input("Enter your username: ")
         password = input("Enter your password: ")
+        print()
 
         cur.execute(f"SELECT COUNT(*) FROM BankUsers WHERE username = '{username}' AND password = '{password}'")
         result = cur.fetchone()
 
         if result[0] > 0: # type: ignore
-            print("Sign-in successful!")
-            return
+            print("Sign-in successful!\n")
+            return username
+        
         else:
             print("Invalid username or password. Please try again.")
 
-    print("Maximum sign-in attempts reached. Exiting...")
+    print("\nMaximum sign-in attempts reached. Exiting...")
+    sys.exit()
 
+
+def balance(username):
+    cur.execute(f"SELECT COUNT(*) FROM BankUsers WHERE username = '{username}'")
+    result = cur.fetchone()
+
+    if result[0] == 0:
+        print("User not found.")
+        return
+
+    cur.execute(f"SELECT balance FROM BankAccount WHERE username = '{username}'")
+    balance_result = cur.fetchone()
+
+    if not balance_result:
+        print("Error: Unable to retrieve account balance.")
+        return
+
+    balance = balance_result[0]
+
+    data = [[username, balance]] # type: ignore
+    headers = ["Username", "Balance"]
+    table = tabulate.tabulate(data, headers=headers, tablefmt="pretty")
+
+    print(table)
 
 def withdraw(username):
     amount = int(input("Enter amount to withdraw: "))
@@ -156,3 +192,46 @@ def send(recipient_username):
     print("Money sent successfully.")
     print(f"New sender's balance: {new_sender_balance}")
     print(f"New recipient's balance: {new_recipient_balance}")
+
+
+
+print("=============================")
+print("Welcome to Digital Bank Ltd.!")
+print("=============================")
+print()
+
+res1 = input("What'd you like to do?\n1. Sign up\n2. Sign in\n\n")
+user = ''
+
+if res1 == '1':
+    signUp()
+    print("\nPlease login first.\n")
+    user = signIn()
+
+elif res1 == '2':
+    user = signIn()
+
+
+while True:
+    print("\nWhat'd you like to do?")
+    res2 = input("1. View Balance\n2. Withdraw\n3. Deposit\n4. Send Money\n5. Quit\n\n")
+
+
+    if res2 == '1':
+        balance(user)
+
+    elif res2 == '2':
+        withdraw(user)
+
+    elif res2 == '3':
+        deposit(user)
+
+    elif res2 == '4':
+        send(user)
+
+    elif res2 == '5':
+        print("Terminating the process...")
+        sys.exit()
+    
+    else:
+        print("Invalid choice")
